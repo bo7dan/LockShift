7ee5b894497ce6420dbc45fbb908fa4dbce5c2ac
Bogdan <106177168+bo7dan@users.noreply.github.com>
2026-01-24 14:42:03 +0200

    Refactor lockshift.cpp with proper licensing

--- src/lockshift.cpp
+++ src/lockshift.cpp
@@ -1,3 +1,21 @@
+/*
+ * LockShift
+ * Copyright (C) 2026  bo7dan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <iostream>
 #include <windows.h>
 #include <winuser.h>
@@ -119,4 +137,5 @@ int main() {
     std::cout << "Hooks removed. Exit........." << std::endl;
 
     return 0;
+
 }
